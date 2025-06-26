50a63efa7e48b0a4e89b87e4ff4779598804f595
MoriAya44 <123141170+MoriAya44@users.noreply.github.com>
2025-06-27 01:51:18 +0900

    commitテスト

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/', function () {
-//     return view('app');
-// });
 
 Route::get('/{any?}', function () { // {any?} で / も /hoge も受け止める
     return view('app'); // app.blade.php を返す
