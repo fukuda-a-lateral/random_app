a7a60b61b5f3dd30f1f27e2d3b081416ff65c3bf
MoriAya44 <123141170+MoriAya44@users.noreply.github.com>
2025-07-01 07:44:32 +0900

    importのエイリアス設定

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import laravel from "laravel-vite-plugin";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
     plugins: [
@@ -16,4 +17,12 @@ export default defineConfig({
         }),
         tailwindcss(),
     ],
+    resolve: {
+        alias: {
+            // @styles というエイリアス名で resources/scss/styles ディレクトリを指すように設定
+            "@styles": path.resolve(__dirname, "resources/scss/styles"),
+            // もし必要なら、@components なども追加できる
+            "@components": path.resolve(__dirname, "resources/ts/components"),
+        },
+    },
 });
