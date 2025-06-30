ea31eb7d2dd9cf72b7f9e949698bfe6eba351284
MoriAya44 <123141170+MoriAya44@users.noreply.github.com>
2025-07-01 08:41:04 +0900

    Breezeの設定

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,17 @@
 
 use Illuminate\Support\Facades\Route;
 
-
 Route::get('/{any?}', function () { // {any?} で / も /hoge も受け止める
     return view('app'); // app.blade.php を返す
 })->where('any', '.*');
+
+//BreezeAPIインストールした際の自動記述
+//Laravel側をAPI特化で使う想定でhtmlではなくてただLaravelのバージョンを返すだけにしている
+// Route::get('/', function () {
+//     return ['Laravel' => app()->version()];
+// });
+
+
+
+
+// require __DIR__ . '/auth.php';
