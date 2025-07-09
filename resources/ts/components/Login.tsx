47123d2955af889038aa1af542ef53a7fc409183
MoriAya44 <123141170+MoriAya44@users.noreply.github.com>
2025-07-10 07:32:49 +0900

    ログインフォームの作成

--- resources/ts/components/Login.tsx
+++ resources/ts/components/Login.tsx
@@ -1,17 +1,52 @@
-import { Button, PasswordInput } from "@mantine/core";
+import { Box, Button, PasswordInput, TextInput } from "@mantine/core";
+import { useState } from "react";
+import { useForm } from "@mantine/form";
 
 export function Login() {
+    const form = useForm({
+        mode: "uncontrolled",
+        initialValues: {
+            email: "",
+            pass: false,
+        },
+        validate: {
+            email: (value) =>
+                /^\S+@\S+$/.test(value) ? null : "その入力じゃだめ！",
+        },
+    });
+
+    //これらのステート管理がMantineのuseFormを使うことで不要になる
+    // const [login_id, LoginId] = useState("");
+    // const [login_pass, LoginPass] = useState("");
+    const handleSubmit = (values: typeof form.values) => {
+        console.log("入力値はuseFormがいい感じにセットしてくれる", values);
+        // ここで認証のapiを呼び出す
+    };
     return (
         <div style={{ padding: 40 }}>
-            <h1>ログイン</h1>
-            <PasswordInput
-                label="Input label"
-                description="Input description"
-                placeholder="Input placeholder"
-            />
-            <Button color="grape" size="lg" className="text-red">
-                グレープボタン🍇
-            </Button>
+            <form onSubmit={form.onSubmit(handleSubmit)}>
+                <Box p={"xs"}>
+                    <TextInput
+                        {...form.getInputProps("email")}
+                        label="ログインID"
+                        placeholder="ログインID or メールアドレス"
+                    />
+                    <PasswordInput
+                        {...form.getInputProps("pass")}
+                        label="パスワード"
+                        placeholder="ダルだるちっちくん"
+                    />
+                </Box>
+                <Button
+                    color="grape"
+                    size="lg"
+                    className="text-red"
+                    type="submit"
+                    onClick={Login}
+                >
+                    ログインボタン
+                </Button>
+            </form>
         </div>
     );
 }
